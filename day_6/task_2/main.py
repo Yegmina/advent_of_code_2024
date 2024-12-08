039dfff7a1ea82b73aa590f3606f987b03914d64
Yehor <te35603560@gmail.com>
2024-12-09 00:09:06 +0200

    day_6(task_2 some problem, return 0)

--- day_6/task_2/main.py
+++ day_6/task_2/main.py
@@ -1,3 +1,6 @@
+import copy
+
+
 def read_input_as_grid(filename):
     """
     Reads the input file line by line and converts it into a 2D list.
@@ -7,42 +10,33 @@ def read_input_as_grid(filename):
         local_grid = [list(line.strip()) for line in lines]  # Converting each line to a list of characters
     return local_grid
 
-def print_test(local_grid):
-    for row in local_grid:
-        print(row)
-    print("full grid=")
-    print(pretty_grid(local_grid))
-
-def print_grid(local_grid):
-    for row in local_grid:
-        print(row)
-
 def pretty_grid(local_grid):
-    """Replacing symbols to human language words, just to make it look better"""
+    """Replacing symbols to human-readable words for better visualization."""
     local_pretty_grid = []
     for row in local_grid:
-        pretty_row=[]
+        pretty_row = []
         for element in row:
             match element:
                 case ".":
-                    pretty_element="empty"
-
+                    pretty_element = "empty"
                 case "#":
-                    pretty_element="barrier"
+                    pretty_element = "barrier"
                 case "^":
-                    pretty_element="guardUp"
+                    pretty_element = "guardUp"
+                case "<":
+                    pretty_element = "guardLeft"
+                case ">":
+                    pretty_element = "guardRight"
+                case "v":
+                    pretty_element = "guardDown"
                 case _:
-                    print(f"Unexpected symbol, {element}")
-                    pretty_element="?"
+                    pretty_element = "unknown"
             pretty_row.append(pretty_element)
         local_pretty_grid.append(pretty_row)
-
     return local_pretty_grid
 
-
-
 def short_grid(pretty_grid):
-    """Replacing human language words back to symbols."""
+    """Converts the human-readable grid back to symbols."""
     local_short_grid = []
     for row in pretty_grid:
         short_row = []
@@ -52,202 +46,180 @@ def short_grid(pretty_grid):
                     short_element = "."
                 case "barrier":
                     short_element = "#"
+                case "guardUp":
+                    short_element = "^"
+                case "guardLeft":
+                    short_element = "<"
+                case "guardRight":
+                    short_element = ">"
+                case "guardDown":
+                    short_element = "v"
                 case "path":
                     short_element = "X"
-                case "guardUp":
-                    print("initial guard up")
-                    short_element = "^"
-                case "^" | "v" | ">" | "<":
-                    print(f"guard found {element}")
-                    short_element = element
                 case _:
-                    print(f"Unexpected symbol {element}")
                     short_element = "?"
             short_row.append(short_element)
         local_short_grid.append(short_row)
-
     return local_short_grid
-
-
-
-
 class Guard:
     def __init__(self, local_grid):
         self.grid = local_grid
-        self.x=[]  # list   of X coordinates where guard was
-        self.y=[]  # list   of Y coordinates where guard was
-        self.directions=[]
-        self.current_x=-1
-        self.current_y=-1
-        self.direction="Up"
-        self.out_of_map=False
+        self.visited_positions = set()
+        self.visited_states = set()
+        self.current_x = -1
+        self.current_y = -1
+        self.direction = "Up"
+        self.out_of_map = False
         self.find_guard()
-        self.max_x=len(self.grid)-1
-        self.max_y=len(self.grid[0])-1
-        self.looped=False
-        self.path_counted=-1
-
-
+        self.max_x = len(self.grid[0]) - 1
+        self.max_y = len(self.grid) - 1
+        self.looped = False
 
     def find_guard(self):
-        for i_row, row in enumerate(self.grid):
-            for i_element, element in enumerate(row):
-                match element:
-                    case "guardUp":
-                        self.current_y=i_row
-                        self.current_x=i_element
-                        self.direction="Up"
-                    case "guardDown":
-                        self.current_y=i_row
-                        self.current_x=i_element
-                        self.direction="Down"
-                    case "guardLeft":
-                        self.current_y=i_row
-                        self.current_x=i_element
-                        self.direction="Left"
-                    case "guardRight":
-                        self.current_y=i_row
-                        self.current_x=i_element
-                        self.direction="Right"
-
-
-    def print_info(self):
-        print("current x="+str(self.current_x))
-        print("current y="+str(self.current_y))
-        print("direction="+str(self.direction))
-        #print("max_x="+str(self.max_x))
-        #print("max_y="+str(self.max_y))
-
+        for y, row in enumerate(self.grid):
+            for x, element in enumerate(row):
+                if element in "^v<>":
+                    self.current_y = y
+                    self.current_x = x
+                    self.direction = {
+                        "^": "Up",
+                        "v": "Down",
+                        "<": "Left",
+                        ">": "Right"
+                    }[element]
 
     def move(self):
         self.check_and_redirect()
         self.mark_path()
 
+        # Save the current state
+        state = (self.current_x, self.current_y, self.direction)
+        if state in self.visited_states:
+            self.looped = True
+            return
+        self.visited_states.add(state)
+
+        # Move based on direction
         match self.direction:
             case "Up":
-                self.move_up()
+                self.current_y -= 1
             case "Down":
-                self.move_down()
+                self.current_y += 1
             case "Left":
-                self.move_left()
+                self.current_x -= 1
             case "Right":
-                self.move_right()
-            case _:
-                print("unexpected direction error")
+                self.current_x += 1
 
         self.check_in_area()
-        self.check_looped()
         self.mark_guard()
 
-
-    def check_in_area(self):
-        if (
-                self.current_y < 0
-                        or
-                self.current_y > self.max_y
-                        or
-                self.current_x < 0
-                        or
-                self.current_x > self.max_x
-        ):
-            self.out_of_map = True
-
     def check_and_redirect(self):
-        if not (self.out_of_map or self.looped):
-            try:
-                if (self.direction=="Up") and (self.grid[self.current_y-1][self.current_x]=="barrier"):
-                    self.direction="Right"
-                if (self.direction=="Right") and (self.grid[self.current_y][self.current_x+1]=="barrier"):
-                    self.direction="Down"
-                if (self.direction=="Down") and (self.grid[self.current_y+1][self.current_x]=="barrier"):
-                    self.direction="Left"
-                if (self.direction=="Left") and (self.grid[self.current_y][self.current_x-1]=="barrier"):
-                    self.direction="Up"
-            except Exception as e:
-                print(e)
+        if self.out_of_map:
+            return
 
-    try:
-        def move_up(self):
-            self.current_y=self.current_y-1
-        def move_down(self):
-            self.current_y=self.current_y+1
-        def move_left(self):
-            self.current_x=self.current_x-1
-        def move_right(self):
-            self.current_x=self.current_x+1
-    except Exception as e:
-        print(e)
+        obstacle_ahead = False
+        match self.direction:
+            case "Up":
+                obstacle_ahead = self.current_y > 0 and self.grid[self.current_y - 1][self.current_x] == "#"
+            case "Down":
+                obstacle_ahead = self.current_y < self.max_y and self.grid[self.current_y + 1][self.current_x] == "#"
+            case "Left":
+                obstacle_ahead = self.current_x > 0 and self.grid[self.current_y][self.current_x - 1] == "#"
+            case "Right":
+                obstacle_ahead = self.current_x < self.max_x and self.grid[self.current_y][self.current_x + 1] == "#"
+
+        if obstacle_ahead:
+            self.direction = {
+                "Up": "Right",
+                "Right": "Down",
+                "Down": "Left",
+                "Left": "Up"
+            }[self.direction]
 
     def mark_path(self):
-        if not (self.out_of_map or self.looped):
-            try:
-                self.grid[self.current_y][self.current_x]="path"
-                self.x.append(self.current_x)
-                self.y.append(self.current_y)
-                self.directions.append(self.direction)
-            except Exception as e:
-                print(e)
-
+        if not self.out_of_map:
+            self.visited_positions.add((self.current_x, self.current_y))
+            self.grid[self.current_y][self.current_x] = "X"
 
     def mark_guard(self):
-        if not (self.out_of_map or self.looped):
-            try:
-                match self.direction:
-                    case "Up":
-                        self.grid[self.current_y][self.current_x]="^"
-                    case "Down":
-                        self.grid[self.current_y][self.current_x]="v"
-                    case "Left":
-                        self.grid[self.current_y][self.current_x]="<"
-                    case "Right":
-                        self.grid[self.current_y][self.current_x]=">"
-                    case _:
-                        print(f"unexpected direction error")
-            except Exception as e:
-                print(e)
+        if not self.out_of_map:
+            self.grid[self.current_y][self.current_x] = {
+                "Up": "^",
+                "Down": "v",
+                "Left": "<",
+                "Right": ">"
+            }[self.direction]
 
-    def check_looped(self):
-        if self.current_x in self.x:
-                local_index=self.x.index(self.current_x)
-                if self.current_y == self.y[local_index] and self.direction==self.directions[local_index]:
-                    self.looped=True
-                    return True
-        return False
+    def check_in_area(self):
+        if (self.current_y < 0 or self.current_y > self.max_y or
+            self.current_x < 0 or self.current_x > self.max_x):
+            self.out_of_map = True
 
-    def path_counter(self):
-        self.path_counted=0
-        for row in self.grid:
-            for element in row:
-                if element=="path":
-                    self.path_counted+=1
-        return self.path_counted
+    def count_path(self):
+        return len(self.visited_positions)
 
+    def how_many_can_be_looped_by_one_more_barrier(self):
+        loop_count = 0
+
+        # Iterate over each position in the guard's path
+        for y, row in enumerate(self.grid):
+            for x, element in enumerate(row):
+                cloned_grid = copy.deepcopy(self.grid)
+
+            # Add a barrier at the position
+                cloned_grid[y][x] = "#"
+
+                print(f"Testing with barrier added at ({x}, {y}):")
+                for row in cloned_grid:
+                    print("".join(row))
+
+                # Create a new guard instance with the modified grid
+                test_guard = Guard(cloned_grid)
+
+                # Simulate the guard's movement
+                while not test_guard.out_of_map and not test_guard.looped:
+                    test_guard.move()
+
+                # Check and log whether the guard entered a loop
+                if test_guard.looped:
+                    print(f"Barrier at ({x}, {y}) caused a loop!")
+                    loop_count += 1
+                else:
+                    print(f"Barrier at ({x}, {y}) did NOT cause a loop.")
+
+        print(f"Total positions causing a loop: {loop_count}")
+        return loop_count
 
 
 def main1():
-    grid = read_input_as_grid("input.txt")
-    #print_test(grid)
-    grid=pretty_grid(grid)
-    guard=Guard(grid)
-    print_grid(short_grid(guard.grid))
-    #guard.print_info()
-    while not (guard.out_of_map or guard.looped):
+    grid = read_input_as_grid("test.txt")
+    guard = Guard(grid)
+
+    while not guard.out_of_map and not guard.looped:
         guard.move()
-        #print_grid(short_grid(guard.grid))
-    guard.path_counter()
 
-    print(guard.path_counted)
+    if guard.looped:
+        print("The guard has entered a loop!")
+    else:
+        print("Distinct positions visited:", guard.count_path())
 
+
+def main2():
+    grid = read_input_as_grid("test.txt")
+    guard = Guard(grid)
+
+    while not guard.out_of_map and not guard.looped:
+        guard.move()
+
+    if guard.looped:
+        print("The guard has entered a loop!")
+    else:
+        print("Distinct positions visited:", guard.count_path())
+
+    # Check how many loops can be caused by adding one more barrier
+    possible_loops = guard.how_many_can_be_looped_by_one_more_barrier()
+    print("Number of positions where adding a barrier causes looping:", possible_loops)
 
 
 if __name__ == "__main__":
-    """
-    grid = read_input_as_grid("test.txt")
-    grid=pretty_grid(grid)
-    guard=Guard(grid)
-    print_grid(short_grid(guard.grid))
-    """
-    main1()
-
-
-    #guard.print_info()
+    main2()
