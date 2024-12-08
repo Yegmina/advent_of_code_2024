c39ff8ba6418e80433fd70d873f49e2529716164
Yehor <te35603560@gmail.com>
2024-12-08 16:05:39 +0200

    day_6(pretty_grid function)

--- day_6/main.py
+++ day_6/main.py
@@ -7,13 +7,36 @@ def read_input_as_grid(filename):
         grid = [list(line.strip()) for line in lines]  # Converting each line to a list of characters
     return grid
 
-
-
-if __name__ == "__main__":
-    grid = read_input_as_grid("input.txt")
+def print_test(grid):
     for row in grid:
         print(row)
     print("full grid=")
-    print(grid)
-    print(grid[0])
-    print(grid[0][4])
+    print(pretty_grid(grid))
+
+
+
+def pretty_grid(grid):
+    """Replacing symbols to human language words, just to make it look better"""
+    pretty_grid = []
+    for row in grid:
+        pretty_row=[]
+        for element in row:
+            match element:
+                case ".":
+                    pretty_element="empty"
+
+                case "#":
+                    pretty_element="barrier"
+                case "^":
+                    pretty_element="guardUp"
+                case _:
+                    print(f"Unexpected symbol, {element}")
+                    pretty_element="?"
+            pretty_row.append(pretty_element)
+        pretty_grid.append(pretty_row)
+
+    return pretty_grid
+
+if __name__ == "__main__":
+    grid = read_input_as_grid("input.txt")
+    print_test(grid)
