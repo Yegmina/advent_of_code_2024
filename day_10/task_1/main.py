361e16272821cf0bacf603124eda29219050feb2
Yehor <te35603560@gmail.com>
2024-12-14 21:58:03 +0200

    day_10(task_1 give too big result, searching for bug)

--- day_10/task_1/main.py
+++ day_10/task_1/main.py
@@ -27,7 +27,7 @@ class Grid:
             print(row)
 
 class Mover(Grid):
-    """for path counting, mover='person who move' from 0 to 9 and calculate ways """
+    """For path counting, mover='person who moves' from 0 to 9 and calculate ways."""
 
     def __init__(self, grid):
         super().__init__(grid)
@@ -50,7 +50,7 @@ class Mover(Grid):
         print("End points:", self.end_points)
 
     def is_out_of_grid(self, x, y):
-        """Check if a position is within the grid bounds for certain x and y as parameters."""
+        """Check if a position is out of the grid bounds."""
         local_within_x_bounds = 0 <= x < len(self.grid[0])
         local_within_y_bounds = 0 <= y < len(self.grid)
 
@@ -58,6 +58,90 @@ class Mover(Grid):
             return True
 
         return False
+
+    def paths_from_one(self, start_point):
+        """
+        Perform mover to explore all paths starting from a given point.
+        Return the number of unique height 9 positions reachable.
+        """
+        queue = [start_point]
+        visited = set()
+        reachable_nines = 0
+
+        while queue:
+            current_point = queue.pop(0)
+
+            if current_point in visited:
+                continue
+            visited.add(current_point)
+
+            current_x, current_y = current_point
+
+            # Check if the current point is height 9
+            if self.grid[current_y][current_x] == 9:
+                reachable_nines += 1
+                continue
+
+            # Add valid neighbors to the queue using direction functions
+            if self.can_left(current_point):
+                queue.append((current_x - 1, current_y))
+            if self.can_right(current_point):
+                queue.append((current_x + 1, current_y))
+            if self.can_up(current_point):
+                queue.append((current_x, current_y - 1))
+            if self.can_down(current_point):
+                queue.append((current_x, current_y + 1))
+
+        return reachable_nines
+
+    def paths_from_all(self):
+        """return all sums of paths' nines reacheble"""
+        """in short: sum of the scores of all trailheads"""
+        local_sum=0
+        for start_point in self.start_points:
+            local_sum += self.paths_from_one(start_point)
+
+        return local_sum
+
+
+    def can_move_one_step(self, local_current_point, local_next_point):
+        """Sub function for can_left, can_right etc."""
+        local_current_x, local_current_y = local_current_point
+        local_next_x, local_next_y = local_next_point
+
+
+        if not self.is_out_of_grid(local_next_x, local_next_y):
+            local_current_value = self.grid[local_current_y][local_current_x]
+            local_next_value = self.grid[local_next_y][local_next_x]
+            difference_value = local_next_value - local_current_value
+            if difference_value == 1:
+                return True
+
+        return False
+
+    def can_left(self, local_current_point):
+        """Return true if mover can go one step left."""
+        local_current_x, local_current_y = local_current_point
+        local_next_x, local_next_y = local_current_x - 1, local_current_y
+        return self.can_move_one_step((local_current_x, local_current_y), (local_next_x, local_next_y))
+
+    def can_right(self, local_current_point):
+        """Return true if mover can go one step right."""
+        local_current_x, local_current_y = local_current_point
+        local_next_x, local_next_y = local_current_x + 1, local_current_y
+        return self.can_move_one_step((local_current_x, local_current_y), (local_next_x, local_next_y))
+
+    def can_up(self, local_current_point):
+        """Return true if mover can go one step up."""
+        local_current_x, local_current_y = local_current_point
+        local_next_x, local_next_y = local_current_x, local_current_y - 1
+        return self.can_move_one_step((local_current_x, local_current_y), (local_next_x, local_next_y))
+
+    def can_down(self, local_current_point):
+        """Return true if mover can go one step down."""
+        local_current_x, local_current_y = local_current_point
+        local_next_x, local_next_y = local_current_x, local_current_y + 1
+        return self.can_move_one_step((local_current_x, local_current_y), (local_next_x, local_next_y))
 
 
 if __name__ == "__main__":
@@ -67,6 +151,6 @@ if __name__ == "__main__":
     grid_instance.pretty_print()
 
     mover_instance = Mover(grid_instance.grid)
-
     mover_instance.print_SnE_points()
 
+    print(mover_instance.paths_from_all())
